0c87b6160539e0c61efdbaa6e622d9c90898aad4
何拾玖 <cuz_mr.j@live.com>
2022-11-27 03:41:44 +0800

    add SQLite demo framework4.6，framework4.8, Net Core 3.1，Net 5，Net 6

---
+++ demo/SQLite/Log4NetAdoNetAppender.Net48.Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4NetAdoNetAppender.Net48.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
